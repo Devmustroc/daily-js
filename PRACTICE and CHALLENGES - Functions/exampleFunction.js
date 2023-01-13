25a9da7694d18f3d84ae47be74fbe036c8c688bb
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-01-13 15:45:07 +0100

    Practicing JS Function

--- PRACTICE and CHALLENGES - Functions/exampleFunction.js
+++ PRACTICE and CHALLENGES - Functions/exampleFunction.js
@@ -3,6 +3,7 @@
  * Simple function in JS
  */
 
+/*
 function myFun() {}
 console.log(myFun);
 myFun();
@@ -11,3 +12,25 @@ console.log(funcResult); // Undefined
 console.log(myFun()); // Undefined
 
 console.log(myFun(10, true)); // Undefined
+*/
+
+/**
+ * EXAMPLE - 2
+ *
+ * Parameters of the function
+ */
+function myFunc2 (a , b) {
+    let c;
+    c = a;
+    a = b;
+    b = c;
+    console.log(a);
+    console.log(b);
+}
+
+myFunc2(12, 5); // 5 12
+
+
+myFunc2(true, null); // true null
+myFunc2(); // undefined, undefined
+console.log(myFunc2(4, 3)); // undefined
