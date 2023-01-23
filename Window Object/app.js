a4bfef35a60740f1062ac06ecd636343cef04e59
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-01-23 12:10:32 +0100

    Windows Object Overview

---
+++ Window Object/app.js
@@ -0,0 +1,58 @@
+// Window Method / Object / Properties
+
+/*window.console.log(123);*/    // console is a property of window object
+
+// Alert
+/*alert("Hello World");*/
+
+// Prompt
+// const input = prompt();
+// alert(input);
+
+// Confirm
+// if (confirm('Are you sure?')) {
+//     console.log('YES');
+// } else {
+//     console.log('NO');
+// }
+
+let val;
+// Outer height and width
+// val = window.outerHeight;
+// val = window.outerWidth;
+// console.log(val);
+// Inner height and width
+// val = window.innerHeight;
+/*val = window.innerWidth;*/
+
+// Scroll points
+// val = window.scrollY;
+// val = window.scrollX;
+
+// Location Object
+// val = window.location;
+// val = window.location.hostname;
+// val =  window.location.port;
+// val = window.location.href;
+// val = window.location.search;
+
+// Redirect
+// window.location.href= 'http://google.com';
+
+//reload
+// window.location.reload()
+
+// History Object
+
+// window.history.go(-1);
+/*val = window.history.length*/
+
+// Navigator Object
+val = window.navigator;
+val = window.navigator.appName;
+val = window.navigator.appVersion;
+val = window.navigator.userAgent;
+val = window.navigator.platform;
+val = window.navigator.vendor;
+val = window.navigator.language;
+console.log(val);
