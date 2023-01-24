24d8acec99671b245d98d1c87ef39d59c144f5d9
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-01-24 12:39:31 +0100

    Replacing Remove

--- DOM & Manipulation & Event/Creating Elements/app.js
+++ DOM & Manipulation & Event/Creating Elements/app.js
@@ -1,28 +1,53 @@
-// Create element
-const li = document.createElement('li');
+// Replacing elements
 
-//add class
-li.className = 'collection-item';
+// Create Element
+const newHeading = document.createElement('h2');    // Create element
+// Add id
 
-//add id
-li.id = "new-item";
+newHeading.id = 'task-title';   // Add id
+// New text node
+newHeading.appendChild(document.createTextNode('Task List'));   // Add text node
 
-//add attribute
-li.setAttribute("title", "New Item");
+// Get the old heading
+const oldHeading = document.getElementById('task-title');   // Get old heading
 
-//create text node and append
-li.appendChild(document.createTextNode('Hello World'));
+// Parent
+const cardAction = document.querySelector('.card-action');  // Get parent
 
-//create new link element
-const link = document.createElement('a');
-//add classes
-link.className = 'delete-item secondary-content';
-//add icon html
-link.innerHTML = '<i class="fa fa-remove"></i>';
-//append link into li
-li.appendChild(link);
+cardAction.replaceChild(newHeading, oldHeading);    // Replace
 
-// Append li as child to ul
-document.querySelector('ul.collection').appendChild(li);
-console.log(li);
+// console.log(newHeading);
 
+// Remove element
+const lis = document.querySelectorAll('li');    // Get all li
+const list = document.querySelector('ul');  // Get ul
+
+// Remove list item
+lis[0].remove();    // Remove first li
+
+// Remove Child element
+list.removeChild(lis[3]);   // Remove 4th li
+
+// Classes & Attributes
+const firstli = document.querySelector("li:first-child");   // Get first li
+const link = firstli.children[0];   // Get first li's child
+//  Classes
+let val;    // Declare variable
+val = link.className;   // Get class name
+val = link.classList;   // DOMTokenList
+val = link.classList[0];    // first class
+link.classList.add('test'); // add class
+link.classList.remove('test');  // remove class
+val = link;
+
+// Attributes
+val = link.getAttribute('href');    // get attribute
+val = link.setAttribute('href', 'https://google.com');  // set attribute
+link.setAttribute('title', 'google');   // set attribute
+val = link.hasAttribute('title');   // check attribute
+link.removeAttribute('title');  // remove attribute
+val = link;   // log
+
+
+
+console.log(val);
